75a617e4e71ecbe0adf242ce7347beafc50a5f9f
liuxiaohong <liuxiaohong@kokozu.net>
2017-05-11 11:10:26 +0800

    提交domain，dao，service结构

---
+++ dao/src/main/java/language/App.java
@@ -0,0 +1,13 @@
+package language;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
