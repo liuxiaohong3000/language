7793b649e5e31dd5eae9f5fdd3a98a6daebd4f41
liuxiaohong <liuxiaohong@kokozu.net>
2017-05-17 11:27:25 +0800

    增加配置服务

---
+++ config/src/main/java/com/language/config/Application.java
@@ -0,0 +1,21 @@
+package com.language.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class Application {
+    private static Logger log = LoggerFactory.getLogger(Application.class);
+    /**
+     * Main Start
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+    }
+
+}
