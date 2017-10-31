97e4ae480e7e5d923472c9a2e4c80d006a45eda8
liuxiaohong <liuxiaohong@kokozu.net>
2017-10-31 14:43:07 +0800

    增加test

--- word/src/test/java/com/language/word/service/busservice/BusWordServiceTest.java
+++ word/src/test/java/com/language/word/service/busservice/BusWordServiceTest.java
@@ -31,5 +31,32 @@ public class BusWordServiceTest {
         Results results=busWordService.listTypes();
         System.out.println(JSON.toJSONString(results));
     }
+    @Test
+    public void addWords() {
+        String wordArray[]=getWordArray();
+        int failnum=0;
+        for(String word : wordArray){
+            try{
+                int leftIndex=word.indexOf("[");
+                int rightIndex=word.indexOf("]");
+                String name=word.substring(0,leftIndex).trim();
+                String pronunciation=word.substring(leftIndex,rightIndex+1).trim();
+                String detail=word.substring(rightIndex+1).trim();
+                busWordService.addWord(name,detail,pronunciation,70,"",detail,detail,"","");
+            }catch(Exception e){
+                //e.printStackTrace();
+                failnum++;
+                System.out.println("---"+word+"---"+failnum);
+            }
+        }
+    }
+    public String[] getWordArray()
+    {
+        StringBuilder aa=new StringBuilder();
+        aa.append("a	[ei]	art.一(个)；每一(个)；(同类事物中)任一个@@@");
+
+
+        return aa.toString().split("@@@");
+    }
 
 }
